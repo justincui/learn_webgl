e6a8763b3fc8c6ad1c1d181629f43e8b8e8a14dd
Justin Cui <justincui@gmail.com>
2017-01-21 17:48:36 -0800

    rewrite the read_file with generator

--- src/HelloCanvas.js
+++ src/HelloCanvas.js
@@ -1,16 +1,21 @@
 /**
  * Created by justincui on 1/21/17.
  */
-function main() {
+function main(){
+    Promise.all(read_file('shader/vs/vertex.c', 'shader/fs/fragment.c'))
+        .then((shaders_src)=>main_routine(shaders_src[0], shaders_src[1]));
+
+}
+function main_routine(VSHADER_SRC, FSHADER_SRC) {
     var canvas = document.getElementById('webgl');
     var gl = getWebGLContext(canvas);
     if(!gl){
         console.log('Failed to get rendering context for WebGL');
         return;
     }
-    const [VSHADER_SRC, FSHADER_SRC] = read_file(['shader/vs/vertex.c', 'shader/fs/fragment.c']);
-    console.log('vertex shader=\n',VSHADER_SRC);
-    console.log('fragment shader=\n', FSHADER_SRC);
+
+    // console.log('vertex shader=\n',VSHADER_SRC);
+    // console.log('fragment shader=\n', FSHADER_SRC);
 
     if(!initShaders(gl, VSHADER_SRC, FSHADER_SRC)){
         console.log('failed to initialize shaders');
